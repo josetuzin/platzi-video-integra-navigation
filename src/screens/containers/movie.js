e5fc0dfc488cdb25c6b79eba997ce612e1bc1c19
José Tuzinkievicz <josetuzin@gmail.com>
2019-11-21 10:58:14 -0300

    clase 33 agregando descripcion a la pelicula

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -3,6 +3,7 @@ import MovieLayout from '../components/movie'
 import Player from '../../player/containers/player'
 import Header from '../../sections/components/header'
 import Close from '../../sections/components/close'
+import Details from '../../videos/components/details'
 import { connect } from 'react-redux'
 
 class Movie extends Component {
@@ -23,9 +24,15 @@ class Movie extends Component {
                     />
                 </Header>
                 <Player />
+                <Details {...this.props.movie} />
             </MovieLayout>
         )
     }
 }
 
-export default connect(null)(Movie)
+function mapStateToProps(state) {
+    return {
+        movie: state.selectedMovie
+    }
+}
+export default connect(mapStateToProps)(Movie)
